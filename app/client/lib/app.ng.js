43a4e1908e3ae5058ebc0653c02ced91a3a97e6e
Marc Stark <self@marcstark.com>
2015-12-28 20:33:29 +0100

    Add Angular UI Bootstrap

--- app/client/lib/app.ng.js
+++ app/client/lib/app.ng.js
@@ -10,7 +10,7 @@ Meteor.subscribe("userData");
 // Module definitions and organization
 // ********************************************************************************
 
-angular.module('amw-packages',    ['angular-meteor', 'ui.router', 'pascalprecht.translate']);
+angular.module('amw-packages',    ['angular-meteor', 'ui.router', 'ui.bootstrap', 'pascalprecht.translate']);
 angular.module('amw-filters',     []);
 angular.module('amw-directives',  []);
 angular.module('amw-services',    []);
